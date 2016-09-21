6f46d9e9822899fff3843ecee9e4bbf375ef4385
Bagaric <bagaricjos@gmail.com>
2016-09-21 14:37:20 +0200

    Modified the package name

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='docx_templating',
     version='0.0.1',
     description='Library for exporting Word files in the desired template.',
     long_description=readme,
     author='Josip Bagaric',
     author_email='bagaricjos@gmail.com',
-    url='https://github.com/Bagaric',
+    url='https://github.com/Bagaric/docx_templating',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
